eb12df6534ee50779616decfb68cc5e822823a7f
MANISHGHGH <95271062+MANISHGHGH@users.noreply.github.com>
2023-09-12 20:38:05 +0530

    Add files via upload

---
+++ brand.php
@@ -0,0 +1,3 @@
+<?php
+	$domainName = "convertpngtojpg.gq";
+?>
---
+++ header.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="./" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <svg class="bi me-2" width="32" height="32"><img src='./img/icon.png' style='width: 32px; margin-right: 5px;'/></svg>
+        <h1 class="fs-4" style='text-weight: normal;'><?php echo $domainName; ?></h1>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="./" class="nav-link active" aria-current="page">Home</a></li>
+
+      
+<li class="nav-item"><a href="http://localhost/compressjpg/" class="nav-link">Compressjpg</a></li>
+        <li class="nav-item"><a href="http://localhost/jpgtopng/about-us.php" class="nav-link">About Us</a></li>
+ <li class="nav-item"><a href="http://pngtojpg.unaux.com/?i=1" class="nav-link">Jpgtopng</a></li>
+        <li class="nav-item"><a href="http://localhost/jpgtopng/contact-us.php" class="nav-link">Contact Us</a></li>
+        <li class="nav-item"><a href="http://localhost/jpgtopng/privacy-policy.php" class="nav-link">Privacy Policy</a></li>
+      </ul>
+    </header>
+</div>
